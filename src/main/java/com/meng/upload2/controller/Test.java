383db9bb5e00af6460a995406cfdaa623bf4aab6
mengxiangtao <774161192@qq.com>
2018-05-16 14:23:15 +0800

    第一次提交

---
+++ src/main/java/com/meng/upload2/controller/Test.java
@@ -0,0 +1,11 @@
+package com.meng.upload2.controller;
+
+/**
+ * @author NBUG
+ * @date 2018/5/16 14:22
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
